0f435bd3b4752de1a2a7294bf0bf6c29fb5de11c
Vedant <vedantjadhav5566@gmail.com>
2021-04-06 14:32:00 +0530

    Added first name and last name variable

--- forms.py
+++ forms.py
@@ -3,6 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
     username = StringField("Username", validators=[DataRequired(), Length(min=3, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=20)])
