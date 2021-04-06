d69de3731c160aea9e1a9e73cd982e4c6fe43056
Vedant <vedantjadhav5566@gmail.com>
2021-04-06 23:21:42 +0530

    Added custom email and username validators

--- app/forms.py
+++ app/forms.py
@@ -1,15 +1,27 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 class RegistrationForm(FlaskForm):
-    first_name = StringField("First Name", validators=[DataRequired()])
-    last_name = StringField("Last Name", validators=[DataRequired()])
+    first_name = StringField("First Name")
+    last_name = StringField("Last Name")
     username = StringField("Username", validators=[DataRequired(), Length(min=3, max=20)])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired(), Length(min=6, max=20)])
     conf_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username is already taken. Please choose a different username.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email is already taken. Please choose a different email.")
+
 
 class LoginForm1(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
