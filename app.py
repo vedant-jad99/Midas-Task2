352e1fe55dd721832143802d48e5e2c7757d1b3a
Vedant <vedantjadhav5566@gmail.com>
2021-04-07 23:07:29 +0530

    Modified the log file path so that it can be set even if program ran out of source directory

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import logging
 from app import app
 
 if __name__ == '__main__':
-    logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", filename="logs/app.log", level=logging.DEBUG)
+    app_path = '/'.join(__file__.split('/')[:-1])
+    logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", filename=app_path + "/logs/app.log", level=logging.DEBUG)
     logging.info("Starting app")
     app.run(debug=True)
     logging.info("Ending app")
