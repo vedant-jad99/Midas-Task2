e1e1a5aea7ec19fc8381672e52c2036c6d860303
Vedant <vedantjadhav5566@gmail.com>
2021-04-07 23:09:05 +0530

    Added user login, user authentication and logout routes and features

--- app/routes.py
+++ app/routes.py
@@ -2,14 +2,17 @@ from flask import render_template, url_for, flash, redirect
 from app import app, bcrypt, db
 from app.forms import RegistrationForm, LoginForm1, LoginForm2
 from app.models import User
+from flask_login import login_user, current_user, logout_user
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("main.html")
+    return render_template("main.html", user=None)
 
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         app.logger.info("All information valid")
@@ -26,15 +29,34 @@ def registration():
 
 @app.route("/login-username", methods=["GET", "POST"])
 def login1():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm1()
     if form.validate_on_submit():
-        return redirect(url_for('home'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home', user=user))
+        else:
+            flash('Login unsuccessful. Please check username and password.', 'danger')
     return render_template("username-login.html", title="Login", form=form)
 
 @app.route("/login-email", methods=["GET", "POST"])
 def login2():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm2()
     if form.validate_on_submit():
         app.logger.info("All information valid")
-        return redirect(url_for("home"))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home', user=user))
+        else:
+            flash('Login unsuccessful. Please check email and password.', 'danger')
     return render_template("email-login.html", title="Login", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
