277a1bff02a2b8dee9e1e21337fed02a00f40a7b
Vedant <vedantjadhav5566@gmail.com>
2021-04-10 04:33:58 +0530

    Modified database URI

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'ff127ef745c8859dd6509413570a5bd3'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://lkhcaehjtjrbmi:c2e2b58de522d95c1bd8d490f0d278aaf023233c074569403206f8a724e8a6cd@ec2-54-167-168-52.compute-1.amazonaws.com:5432/d678c8ni4pc6mo'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://lkhcaehjtjrbmi:c2e2b58de522d95c1bd8d490f0d278aaf023233c074569403206f8a724e8a6cd@ec2-54-167-168-52.compute-1.amazonaws.com:5432/d678c8ni4pc6mo'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
 login_manager = LoginManager(app)
