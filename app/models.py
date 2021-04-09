a18e80df62ed18c1c8ac4c366ead965df838ce33
Vedant <vedantjadhav5566@gmail.com>
2021-04-09 18:43:08 +0530

    Added a new table for user info and created a relationship

--- app/models.py
+++ app/models.py
@@ -15,6 +15,23 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(40), unique=False, nullable=False)
     account_created = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    info = db.relationship('UserInfo', backref='info', lazy=True)
 
     def __repr__(self):
         return "User details - Name: {}, Username: {}, Email: {}".format(self.first_name + " " + self.last_name, self.username, self.email)
+
+
+class UserInfo(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    about = db.Column(db.String(750), unique=False, nullable=True)
+    link1 = db.Column(db.String(500), unique=False, nullable=True)
+    link2 = db.Column(db.String(500), unique=False, nullable=True)
+    link3 = db.Column(db.String(500), unique=False, nullable=True)
+    link4 = db.Column(db.String(500), unique=False, nullable=True)
+    profession = db.Column(db.String(100), unique=False, nullable=True)
+    profile = db.Column(db.String(100), unique=False, nullable=True, default='profile.png')
+    recovery_mail = db.Column(db.String(100), unique=False, nullable=True)
+    user_id = db.Column(db.String(20), db.ForeignKey('user.id'), unique=True, nullable=False)
+
+    def __repr__(self):
+        return "Profession - {} Id- {}".format(self.profession, self.id)
