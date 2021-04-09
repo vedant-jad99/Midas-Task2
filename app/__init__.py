02fe1d307c273e803168948ff5b7a0eb05806539
Vedant <vedantjadhav5566@gmail.com>
2021-04-09 21:36:41 +0530

    Adding Profile and requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'ff127ef745c8859dd6509413570a5bd3'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://root:Password123#@!@localhost/sitedb'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
 login_manager = LoginManager(app)
